0f2cfc319d9af8e8d714f370c75c41351bd529be
samuvel ramesh <62345838+samsharpy@users.noreply.github.com>
2020-04-01 20:20:35 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,9 @@
+#created on 09/09/2019
+#author:samuvel
+a=int(input("enter the year"))#getting the year to be found
+while(a%400=0 and a!=0 and a is a*24): #chaeaking the entry
+    print("its leap year")
+else:
+    print("its not")#printing the output
+    
+    
